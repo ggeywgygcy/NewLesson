c522881f0fed90c44500a8ba7dd6a6ee50aa8e1f
ggeywgygcy <wasilij18bus@gmail.com>
2024-06-16 12:43:20 +0300

    theory of arrays

--- JS1/Arrays and objects/app.js
+++ JS1/Arrays and objects/app.js
@@ -19,17 +19,12 @@ const listElement = document.getElementById('list')
 const notes = ['Первый заезд', 'Второй заезд']
 
 function render(){
-    listElement.insertAdjacentHTML('beforeend',`
-        <li
-            class="list-group-item d-flex justify-content-between align-items-center"
-            >
-            <span>${notes[0]}</span>
-            <span>
-                <span class="btn btn-small btn-success">&check;</span>
-                <span class="btn btn-small btn-danger">&times;</span>
-            </span>
-            </li> 
-        `)
+    listElement.insertAdjacentHTML('beforeend',
+        getNoteTemplate(notes[0])
+    )
+    listElement.insertAdjacentHTML('beforeend',
+        getNoteTemplate(notes[1])
+    )
 }
 render()
 
@@ -40,16 +35,22 @@ createBtn.onclick = function(){
     //listElement.innerHTML = ` 
         
 
-    listElement.insertAdjacentHTML('beforeend',`
+    listElement.insertAdjacentHTML('beforeend',
+        getNoteTemplate(inputElement.value)
+    )
+    inputElement.value = ''   
+}
+
+function getNoteTemplate(title){
+    return`
         <li
             class="list-group-item d-flex justify-content-between align-items-center"
             >
-            <span>${inputElement.value}</span>
+            <span>${title}</span>
             <span>
                 <span class="btn btn-small btn-success">&check;</span>
                 <span class="btn btn-small btn-danger">&times;</span>
             </span>
             </li> 
-        `)
-    inputElement.value = ''   
+        ` 
 }
